e1a4490267840c054253704e16d601e9c23454c1
Diogo Costa <costa.h4evr@gmail.com>
2013-02-15 18:43:41 -0500

    added fixed viewport

--- meems-utils.js
+++ meems-utils.js
@@ -38,6 +38,13 @@ define(function() {
             
             removeClass : function (el, clazz) {
                 el.className = el.className.replace(new RegExp("\\b" + clazz + "\\b"), "");
+            },
+            
+            fixedViewport : function () {
+                var meta = document.createElement("meta");
+                meta.setAttribute("name", "viewport");
+                meta.setAttribute("content", "width=device-width, initial-scale=1, maximum-scale=1");
+                document.head.appendChild(meta);
             }
         }
     };
