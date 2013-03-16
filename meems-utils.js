a9592d902786387c6b22fd29d9e2965f0aa13ac9
Diogo Costa <costa.h4evr@gmail.com>
2013-03-16 17:56:25 +0000

    changed doc tool to YUIDoc, rewrote documentation, relocated some files and improved scoping of some variables.

--- meems-utils.js
+++ meems-utils.js
@@ -1,4 +1,8 @@
 /*global define*/
+/**
+ * Contains a set of methods that facilitate several tasks.
+ * @module meems-utils
+ */
 define(function () {
     "use strict";
 
@@ -8,26 +12,127 @@ define(function () {
         pendingHtmlMods = {},
         elmId = 0,
         elementsById = {};
-    
+
     var Utils = {
         /**
-         * Used for extending classes.
+         * Methods related with function manipulation.
+         *
+         * @class Fn
          */
-        extend : function (newClass, ClassToExtend, newMethods) {
-            if (typeof newClass === 'function') {
-                newClass.prototype = new ClassToExtend();
-                newClass.constructor = newClass;
-                newClass = newClass.prototype;
-            }
-            
-            for (var k in newMethods) {
-                if (newMethods.hasOwnProperty(k)) {
-                    newClass[k] = newMethods[k];
+        Fn : {
+            /**
+             * Used for extending classes.
+
+             * @method extend
+             * @static
+             * @param {Function|Object} newClass The new class that will contain the extended methods.
+             * @param {Function} ClassToExtend Base class.
+             * @param {Object} newMethods Object with all the new methods.
+             */
+            extend : function (newClass, ClassToExtend, newMethods) {
+                if (typeof newClass === 'function') {
+                    newClass.prototype = new ClassToExtend();
+                    newClass.constructor = newClass;
+                    newClass = newClass.prototype;
                 }
+
+                for (var k in newMethods) {
+                    if (newMethods.hasOwnProperty(k)) {
+                        newClass[k] = newMethods[k];
+                    }
+                }
+            },
+
+            /**
+             * Postpones the invocation of a function until the current method chain is finished.
+             *
+             * @method postPone
+             * @static
+             * @param {Function} fn The function to be called
+             */
+            postPone : function (fn) {
+                setTimeout(fn, 10);
+            },
+
+            /**
+             * Binds the 'this' variable of a function to the given variable.
+             *
+             * @method bind
+             * @static
+             * @param {Function} fn The function to be bind-ed.
+             * @param {Object} self The new 'this'.
+             * @return {Function} The bind-ed function.
+             */
+            bind : function (fn, self) {
+                return function () {
+                    fn.apply(self, arguments);
+                };
+            },
+
+            /**
+             * Ensures a function is called at most once in the given time interval.
+             *
+             * @method throttle
+             * @static
+             * @param {Function} fn The function to be protected.
+             * @param {Number} ms How many milliseconds must pass before the function can be called again.
+             * @return {Function} The protected function.
+             */
+            throttle : function (fn, ms) {
+                var lastCall, now;
+                return function () {
+                    now = (new Date()).getTime();
+                    if (!lastCall || now - lastCall > ms) {
+                        var ret = fn.apply(this, arguments);
+                        lastCall = now;
+                        return ret;
+                    }
+                };
             }
         },
-        
+
+        /**
+         * All methods related to array manipulation.
+         *
+         * @class Array
+         */
+        Array : {
+            /**
+             * Allows to search an array of objects by a property of those objects.
+             *
+             * @method indexOfByProp
+             * @static
+             * @param {Array} arr The array to be searched.
+             * @param {String} prop The name of the property to be matched.
+             * @param {*} value The value to match.
+             * @return {number} The position of the matched element in the array,
+             *                   -1 if none matched the criteria.
+             */
+            indexOfByProp : function (arr, prop, value) {
+                for (var i = 0; i < arr.length; ++i) {
+                    if (arr[i][prop] === value) {
+                        return i;
+                    }
+                }
+
+                return -1;
+            }
+        },
+
+        /**
+         * All methods related to map manipulation.
+         *
+         * @class Map
+         */
         Map : {
+            /**
+             * Extracts all the keys from a map object.
+             *
+             * @method getKeys
+             * @static
+             * @param {Object} map The map to extract keys from.
+             * @return {String[]} Array with the keys.
+             */
             getKeys : function (map) {
                 var keys = [];
                 
@@ -40,8 +145,23 @@ define(function () {
                 return keys;
             }
         },
-        
+
+        /**
+         * All methods related with DOM manipulation.
+         *
+         * @class Dom
+         */
         Dom : {
+            /**
+             * Retrieves the className of an element.
+             * Using this method is required when using other methods from this namespace,
+             * because it will return the most recent changes (even before calling applyChanges).
+             *
+             * @method getClass
+             * @static
+             * @param {HTMLElement} el The element to retrieve the className from.
+             * @return {String} The className.
+             */
             getClass : function (el) {
                 if (!el.$meems_uid) {
                     el.$meems_uid = ++elmId;
@@ -53,17 +173,41 @@ define(function () {
                 
                 return pendingDomClassNameChanges[el.$meems_uid];
             },
-            
+
+            /**
+             * Append a new class name to an element.
+             *
+             * @method addClass
+             * @static
+             * @param {HTMLElement} el The element to add the class name to.
+             * @param {String} clazz The class name to add.
+             */
             addClass : function (el, clazz) {
                 var currentClass = this.getClass(el);
                 pendingDomClassNameChanges[el.$meems_uid] = currentClass.replace(new RegExp("\\b" + clazz + "\\b"), "") + " " + clazz;
             },
-            
+
+            /**
+             * Remove a class name from an element.
+             *
+             * @method removeClass
+             * @static
+             * @param {HTMLElement} el Element to remove the class name from.
+             * @param {String} clazz The class name to remove.
+             */
             removeClass : function (el, clazz) {
                 var currentClass = this.getClass(el);
                 pendingDomClassNameChanges[el.$meems_uid] = currentClass.replace(new RegExp("\\b" + clazz + "\\b"), "");
             },
-            
+
+            /**
+             * Replace the className of an element.
+             *
+             * @method setClass
+             * @static
+             * @param {HTMLElement} el The element to set the className of.
+             * @param {String} clazz The new className.
+             */
             setClass : function (el, clazz)  {
                 if (!el.$meems_uid) {
                     el.$meems_uid = ++elmId;
@@ -71,7 +215,15 @@ define(function () {
                 }
                 pendingDomClassNameChanges[el.$meems_uid] = clazz;
             },
-            
+
+            /**
+             * Set the innerHTML property of an element.
+             *
+             * @method setHtml
+             * @static
+             * @param {HTMLElement} el The element to change.
+             * @param {String} html The new innerHTML property.
+             */
             setHtml : function (el, html) {
                 if (!el.$meems_uid) {
                     el.$meems_uid = ++elmId;
@@ -79,18 +231,14 @@ define(function () {
                 }
                 pendingHtmlMods[el.$meems_uid] = html;
             },
-            
-            fixedViewport : function () {
-                var meta = document.createElement("meta");
-                meta.setAttribute("name", "viewport");
-                meta.setAttribute("content", "width=device-width, initial-scale=1, maximum-scale=1");
-                head.appendChild(meta);
-            },
-            
-            supportsTouch : function () {
-                return supportsTouch;
-            },
-            
+
+            /**
+             * Applies all the changes made by the addClass, setClass, removeClass, setHtml methods.
+             * This is a way to reduce DOM manipulation, speeding up the update process.
+             *
+             * @method applyChanges
+             * @static
+             */
             applyChanges : function () {
                 var newClassName, newHtml, uid;
                 
@@ -115,45 +263,38 @@ define(function () {
                 pendingDomClassNameChanges = {};
                 pendingHtmlMods = {};
                 
+            },
+
+            /**
+             * Add a meta tag to the head element, disabling zooming and fixing the viewport.
+             * Use only when you can't easily add the tag yourself to the HTML file.
+             *
+             * @method fixedViewport
+             * @static
+             */
+            fixedViewport : function () {
+                var meta = document.createElement("meta");
+                meta.setAttribute("name", "viewport");
+                meta.setAttribute("content", "width=device-width, initial-scale=1, maximum-scale=1");
+                head.appendChild(meta);
+            },
+
+            /**
+             * Checks if the browser supports touch events.
+             *
+             * @method supportsTouch
+             * @static
+             * @return {boolean} True if it supports touch events, false otherwise.
+             */
+            supportsTouch : function () {
+                return supportsTouch;
             }
-        },
-        
-        postPone : function (fn) {
-            setTimeout(fn, 10);
-        },
-        
-        bind : function (fn, self) {
-            return function () {
-                fn.apply(self, arguments);
-            };
-        },
-        
-        throttle : function (fn, ms) {
-            var lastCall, now;
-            return function () {
-                now = (new Date()).getTime();
-                if (!lastCall || now - lastCall > ms) {
-                    var ret = fn.apply(this, arguments);
-                    lastCall = now;
-                    return ret;
-                }
-            };
-        },
-        
-        indexOfByProp : function (arr, prop, value) {
-            for (var i = 0; i < arr.length; ++i) {
-                if (arr[i][prop] === value) {
-                    return i;
-                }
-            }
-            
-            return -1;
         }
     };
     
     if (Function.prototype.extend === undefined) {
         Function.prototype.extend = function (baseClass, newMethods) {
-            Utils.extend(this, baseClass, newMethods);
+            Utils.Fn.extend(this, baseClass, newMethods);
         };
     }
     
